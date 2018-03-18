4fa3b0379f609d6bdbbbb453517b09112eead8f7
admin <1015517281@qq.com>
2018-03-18 17:22:37 +0800

    first commit

--- src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
+++ src/main/java/com/roncoo/eshop/one/service/EshopPriceService.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eshop-price-service",fallback = EshopPriceService.class)
+import com.roncoo.eshop.one.service.fallback.EshopPriceServiceFallback;
+
+@FeignClient(value = "eshop-price-service",fallback = EshopPriceServiceFallback.class)
 public interface EshopPriceService {
 
     @RequestMapping(value = "/product-price/findByProductId",method = RequestMethod.GET)
